6799730479243d240b1efce6a818c03ad9421a9d
Hemin Merchant <hemin.merchant@cerner.com>
2014-09-15 12:38:34 -0500

    java unicode

---
+++ sept15.java
@@ -0,0 +1,5 @@
+public class sept15 {
+	public static void main(String[] args) {
+		System.out.println("\u0907\u0902\u091C\u0940\u0928\u093F\u092F\u0930\u093F\u0902\u0917 \u0939\u0947\u0905\u0932\u094D\u200D\u0925\u094D");
+	}
+}
