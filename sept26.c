4d1a05a0053088bce1c222b8eaec8ce0339fdfb4
Hemin Merchant <hemin.merchant@cerner.com>
2014-09-26 14:59:06 -0500

    C obfuscation play

--- sept26.c
+++ sept26.c
@@ -1,11 +1,11 @@
 /* check http://ideone.com/Nne6G9 for output */
-int f(int k,char c){return k?f(k-printf("%c",c),c):0;}
+int f(int k,char c){return k?f(k-printf("%c",c),c):1;}
 int main(){
 	char m[]="nnjfenjddddeeeeeeeeeedeeeeeeeeeedeeeeeeeeeedeeeeeeeeeedenkeeenkdddeeeeeeeeeedeeeeeeeeeedeeeeeeeeeedeeeeeeeeeednneeefneed";
 	char n[]="iinniidddrrimemirirrjlflirjrrjlgkirjrrjlhjirjrimiiiiddrrimjhirirrjlkgirjrrkklfirjrrljmeirliiminiimd";
 	int i,j,k,l;
 	for(i=0;i<11;i++,l=j=k=0,printf("\n"))
 		while(m[i*11+j]-100)
-			k?f(n[i*9+l]-(1<<6)-36,1<<5),l++,k^=1:(f(m[i*11+j]-(1<<7)+28,35),j++,k^=1);
+			k?k^=f(n[i*9+l]-(1<<6)-36,1<<5),l++:(k^=f(m[i*11+j]-(1<<7)+28,35),j++);
 	return 0;
 }
